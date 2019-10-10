e57e371c4daab3e96339f5a9cad5dbe0b9b362c7
maorn <maorn@intensix.com>
2019-10-10 08:28:44 +0300

    adding tests

--- stats/dealer_stats.py
+++ stats/dealer_stats.py
@@ -4,16 +4,17 @@ Created on Oct 8, 2019
 @author: maor
 '''
 import pandas as pd
-from common.cards import BjDeck, dealer_hand
+from common.cards import BjDeck
+from common.game import dealer_hand
 
 
-def dealer_stats_for_given_hand():
+def dealer_stats_for_given_hand(games=1000000, decks=6, save_fn=None):
     dealer_stats = {}
     for i in range(1, 11):
         dealer_stats[str(i)] = {'17': 0, '18': 0, '19': 0, '20': 0, '21': 0, 'F': 0}
 
-    for _ in range(1000000):
-        deck = BjDeck(6)
+    for _ in range(games):
+        deck = BjDeck(decks)
         start_hand = deck.deal()
         dealer = [start_hand]
         count = dealer_hand(dealer, deck)
@@ -21,6 +22,9 @@ def dealer_stats_for_given_hand():
             count = 'F'
         dealer_stats[str(start_hand)][str(count)] = dealer_stats[str(start_hand)][str(count)] + 1.
     raw_count = pd.DataFrame(dealer_stats)
-    raw_count.to_csv('count.csv')
+    print(raw_count)
     total = raw_count / raw_count.sum(axis=0)
-    total.to_csv('percent.csv')
+    print(total)
+    if save_fn is not None:
+        raw_count.to_csv(save_fn + 'count.csv')
+        total.to_csv(save_fn + 'percent.csv')
