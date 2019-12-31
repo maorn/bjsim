a4d8926204db44d42d7a0d0c5b68bb0150ba309f
maorn <maorn@intensix.com>
2019-12-31 17:42:53 +0200

    WIP - turn markov into a class start building the functions inside

--- bjsim/common/mdp.py
+++ bjsim/common/mdp.py
@@ -3,29 +3,31 @@ Created on Nov 5, 2019
 
 @author: maor
 '''
-from bjsim.common.globals import HAND_OPTIONS
 
 
-def init_equal_probs(actions: dict):
-    actions_probs = {}
-    for key, val in actions.items():
-        prob = 1 / len(val)
-        actions_probs[key] = {}
-        for item in val:
-            actions_probs[key][item] = prob
-    return actions_probs
+class MDP:
 
+    def __init__(self, actions: dict):
+        self.actions = actions
+        self.actions_probs = self.init_equal_probs()
+        self.actions_counter = self.reset_counters()
 
-def init_counters(actions: dict):
-    actions_couters = {}
-    for key, val in actions.items():
-        actions_couters[key] = {}
-        for item in val:
-            actions_couters[key][item] = {'count': 0, 'reward': 0}
-    return actions_couters
+    def init_equal_probs(self):
+        actions_probs = {}
+        for key, val in self.actions.items():
+            prob = 1 / len(val)
+            actions_probs[key] = {}
+            for item in val:
+                actions_probs[key][item] = prob
+        return actions_probs
 
+    def reset_counters(self):
+        actions_couters = {}
+        for key, val in self.actions.items():
+            actions_couters[key] = {}
+            for item in val:
+                actions_couters[key][item] = {'count': 0, 'reward': 0}
+        return actions_couters
 
-def start_game():
-    actions_probs = init_equal_probs(HAND_OPTIONS)
-    actions_counter = init_counters(HAND_OPTIONS)
-    return actions_probs, actions_counter
+    def add_game(self, final_hand, rewards):
+        return
--- tests/test_mdp.py
+++ tests/test_mdp.py
@@ -3,18 +3,20 @@ Created on Nov 5, 2019
 
 @author: maor
 '''
-from bjsim.common.mdp import start_game
+from bjsim.common.mdp import MDP
 from bjsim.common.cards import BjDeck
-from bjsim.common.game import play_hand, play_game
+from bjsim.common.game import play_game
 from bjsim.common.policies import random_policy
 from bjsim.common.globals import HAND_OPTIONS
 
 
 def test_states():
-    actions_probs, actions_counter = start_game()
-    policy_params = {'actions': HAND_OPTIONS}
-
+    markov = MDP(HAND_OPTIONS)
+    policy_params = {'actions': markov.actions}
+    deck = BjDeck(6)
     for i in range(10000):
-        deck = BjDeck(6)
+
+        deck.start_game()
         final_hand, d_hand, rewards = play_game(deck, [1], random_policy, **policy_params)
-        print(1)
+        markov.add_game(final_hand[0], rewards[0])
+        print(i)
