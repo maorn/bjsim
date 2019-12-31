6a0da7455c0dd3b596cf5b2f75d1e29bcd6b3d2e
unknown <maor.nissan@gmail.com>
2019-12-31 19:41:43 +0200

    work in Progress, still missing dealer cards for decision, will add them in the next commit

--- tests/test_mdp.py
+++ tests/test_mdp.py
@@ -6,18 +6,20 @@ Created on Nov 5, 2019
 from bjsim.common.mdp import MDP
 from bjsim.common.cards import BjDeck
 from bjsim.common.game import play_game
-from bjsim.common.policies import random_policy, max_policy
+from bjsim.common.policies import random_policy, max_policy,\
+    probability_based_policy
 from bjsim.common.globals import HAND_OPTIONS
 
 
 def test_states():
     markov = MDP(HAND_OPTIONS)
     policy_params = {'actions': markov.actions,
-                     'actions_prob': markov.actions_probs}
+                     'actions_prob': markov.actions_probs,
+                     'threshold': 90}
     deck = BjDeck(6)
     start_money = 0
     end_money = start_money
-    for games in range(100000):
+    for games in range(1000000):
 
         deck.start_game()
         final_hand, d_hand, rewards = play_game(deck, [1], random_policy, **policy_params)
@@ -25,14 +27,16 @@ def test_states():
         markov.add_game(final_hand[0], rewards[0])
     print("average win/lost per game: ", (end_money - start_money) / games)
     markov.update_probs()
+    for i in range(5):
+        start_money = 0
+        end_money = start_money
+        for games in range(100000):
 
-    start_money = 0
-    end_money = start_money
-    for games in range(100000):
-
-        deck.start_game()
-        final_hand, d_hand, rewards = play_game(deck, [1], max_policy, **policy_params)
-        end_money = end_money + rewards[0]
-        markov.add_game(final_hand[0], rewards[0])
-    print("average win/lost per game: ", (end_money - start_money) / games)
-    markov.update_probs()
+            deck.start_game()
+            final_hand, d_hand, rewards = play_game(
+                deck, [1], probability_based_policy, **policy_params)
+            end_money = end_money + rewards[0]
+            markov.add_game(final_hand[0], rewards[0])
+        print("average win/lost per game: ", (end_money - start_money) / games)
+        print(markov.actions_probs)
+        markov.update_probs()
